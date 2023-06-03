1f94d4a3addf8c58152d7e0c993865eccfef446c
Temitopesam1 <temitopesam5@gmail.com>
2023-06-03 01:48:51 +0100

    FIX: Recreate package-lock.json file

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ class DBClients {
         const port = process.env.DB_PORT || 27017;
         const database = process.env.DB_DATABASE || 'files_manager';
     
-        this.url = `mongodb://${host}:${port}`;
+        this.url = process.env.uri;
     
         // Use connect method to connect to the server
         this.client = new MongoClient(this.url, { useUnifiedTopology: true });
