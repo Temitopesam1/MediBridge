7e65d52f5058fdfef0c590049da1e4573d8112a5
Temitopesam1 <temitopesam5@gmail.com>
2023-06-04 18:16:26 +0100

    FEAT: Build chat using socket.io

--- server.js
+++ server.js
@@ -1,72 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-// const { auth, requiresAuth } = require('express-openid-connect');
+import handleConnection from './chat/connect';
 
-// const app = express();
+// app.use(express.static(__dirname + '/public'));
 
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: 'a long, randomly-generated string stored in env',
-//   baseURL: 'http://localhost:3000',
-//   clientID: 'QsyA1QqqdPlJpABQUvUMI62nnF6fPwkR',
-//   issuerBaseURL: 'https://dev-bw4lp6xgs8xgjx02.us.auth0.com'
-// };
-
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(config));
-
-// // req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
-
-// // The /profile route will show the user profile as JSON
-// app.get('/profile', requiresAuth(), (req, res) => {
-//     res.send(JSON.stringify(req.oidc.user));
-// });
-
-
-
-
-
-app.use(express.static(__dirname));
-app.use(bodyparser.urlencoded({extended: false}))
-
-const messageSchema = new mongoose.Schema({
-  name: String,
-  message: String
-});
-const Message = mongoose.model('Message', messageSchema);
-
-const dbUrl = process.env.URL;
-
-app.get('/messages', (req, res) =>{
-  Message.find();
-})
-
-app.post('/messages', (req, res) =>{
-  const message = new Message(req.body);
-  message.save();
-  io.emit('message', req.body);
-  res.sendStatus(200);
-})
-
-io.on('connection', () =>{
-  console.log('a user is connected');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-
+io.on('connection', handleConnection);
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`listening on http://localhost:${port}`);
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
