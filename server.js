8c57b9fd0c1a22db7878f4aa2eaf190b009a290d
Temitopesam1 <temitopesam5@gmail.com>
2023-06-07 02:13:46 +0100

    FIX: Correct the Post route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const bodyparser = require('body-parser');
-const mongoose = require('mongoose');
-import Appointment from './db';
+const { google } = require('googleapis');
 
 
 const app = express();
@@ -10,37 +10,91 @@ app.use(bodyparser.urlencoded({extended: false}));
 const port = process.env.PORT || 3000;
 
 
-mongoose.connect('mongodb://localhost/appointments', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Create a new appointment
-app.post('/appointments', async(req, res) => {
-    const appointment = await Appointment.create(req.body);
-    if(appointment){
-        return res.status(201).json({ message: 'Appointment created successfully' });
-    }
-    return res.status(500).json({ error: 'An error occurred while creating the appointment' });
+const jwtClient = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY,
+  process.env.SCOPES
+);
+  
+const calendar = google.calendar({
+  version: 'v3',
+  project: process.env.GOOGLE_PROJECT_NUMBER,
+  auth: jwtClient
 });
   
-// Retrieve all appointments
-app.get('/appointments', async(req, res) => {
-    const appointment = await Appointment.find()
-    if(appointment){
-        return res.status(200).json(appointment);
+app.get('/appointment', (req, res) => {
+  calendar.events.list({
+    calendarId: process.env.GOOGLE_CALENDAR_ID,
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, result) => {
+    if (err) {
+      res.status(500).send('There was an error contacting the Calendar service: ' + err);
+    } else {
+      if (result.data.items.length) {
+        res.status(200).json({ events: result.data.items });
+      } else {
+        res.status(200).json({ message: 'No upcoming events found.' });
+      }
     }
-    return res.status(500).json({ error: 'An error occurred while retrieving appointments' });
+  });
 });
   
-// Cancel an appointment
-app.delete('/appointments/:id', (req, res) => {
-    const appointmentId = req.params;
-    const appointment = Appointment.findByIdAndRemove({_id: appointmentId});
-    if(appointment){
-        return res.status(200).json({ message: 'Appointment cancelled successfully' });
-    }
-    return res.status(500).json({ error: 'An error occurred while cancelling appointment' });
-});
-  
+app.post("/appointment",(req,res)=>{
+  const {
+    patientName,
+    doctorName,
+    startTime,
+    endTime,
+    location,
+    summary,
+    description,
+   } = req.body;
+  var event = {
+    'summary': summary,
+    'location': location,
+    'description': description || summary,
+    'start': {
+      'dateTime': startTime,
+      'timeZone': 'Africa/Lagos',
+    },
+    'end': {
+      'dateTime': endTime,
+      'timeZone': 'Africa/Lagos',
+    },
+    'attendees': [doctorName, patientName],
+    'reminders': {
+      'useDefault': false,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    },
+  };
+    
+  const auth = new google.auth.GoogleAuth({
+    keyFile: process.env.KEYFILE,
+    scopes: 'https://www.googleapis.com/auth/calendar',
+  });
+  auth.getClient().then(a=>{
+    calendar.events.insert({
+      auth:a,
+      calendarId: process.env.GOOGLE_CALENDAR_ID,
+      resource: event,
+    }, function(err, event) {
+      if (err) {
+        res.status(500).send('There was an error contacting the Calendar service: ' + err);
+        return;
+      }
+      res.status(200).send("Event successfully created!");
+    });
+  })
+})
 
 app.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
