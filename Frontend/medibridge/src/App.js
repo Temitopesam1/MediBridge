d17a1967af5bf6c720e7a3bd6f5470215a55554e
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-06-14 21:34:49 +0300

    Changes in the Home file

--- Frontend/medibridge/src/App.js
+++ Frontend/medibridge/src/App.js
@@ -1,23 +1,71 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import LandingPage from './LandingPage';
+import PersonalDashboard from './PersonalDashboard';
+import DoctorReviews from './DoctorsReview';
+import Messages from './Messages';
+import MedicalArticles from './Articles';
+import Appointments from './Appointments';
+import Footer from './Footer';
 
+const Navbar = styled.nav`
+  background: ${(props) => props.theme.navbarBackground};
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+`;
 
+const NavLinks = styled.ul`
+  display: flex;
+  list-style: none;
+`;
 
-function App(){
+const NavLink = styled.li`
+  margin-right: 1rem;
+`;
 
+const App = () => {
+  return (
+    <div>
+    <Router>
+      <Navbar>
+          <NavLinks>
+            <NavLink>
+              <Link to="/">Home</Link>
+            </NavLink>
+            <NavLink>
+              <Link to="/dashboard">Personal Dashboard</Link>
+            </NavLink>
+            <NavLink>
+              <Link to="/doctor-reviews">Doctors Review</Link>
+            </NavLink>
+            <NavLink>
+              <Link to="/messages">Messages</Link>
+            </NavLink>
+            <NavLink>
+              <Link to="/medical-articles">Medical Articles</Link>
+            </NavLink>
+            <NavLink>
+              <Link to="/appointment">Appointments</Link>
+            </NavLink>
+          </NavLinks>
+        </Navbar>
 
-return <> 
-         <BrowserRouter>
-               <Routes>
-                   <Route index path={"/"} element={<Home />} />
-                   <Route path={"/signup"} element={<Signup />} />
-                   <Route path={"/login"} element={<Login />} />
-               </Routes>
-          </BrowserRouter>
-      </>
+      <Routes>
+        <Route exact path="/" element ={<LandingPage/>} />
+        <Route path="/dashboard" element={<PersonalDashboard/>} />
+        <Route path="/doctor-reviews" element={<DoctorReviews/>} />
+        <Route path="/messages" element={<Messages/>} />
+        <Route path="/medical-articles" element={<MedicalArticles/>} />
+        <Route path="/appointment" element={<Appointments/>} />
+      </Routes>
+    </Router>
 
-}
+    <Footer />
+    </div>
+  );
+};
 
 export default App;
+
